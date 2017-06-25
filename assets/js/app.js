b7a2930a7381e35dc515372bb1611911b1b64f4a
Scott Montague <SMontague29@gmail.com>
2017-06-25 17:39:26 -0600

    File Structure/basic page structure

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDk6ULjucP17jw5DtCrnTpOAKp5rZxmT0Q",
+    authDomain: "dallascowboys-ffb12.firebaseapp.com",
+    databaseURL: "https://dallascowboys-ffb12.firebaseio.com",
+    projectId: "dallascowboys-ffb12",
+    storageBucket: "dallascowboys-ffb12.appspot.com",
+    messagingSenderId: "40561398925"
+  };
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+// Variables
